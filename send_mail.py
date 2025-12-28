5976a9c134e0bef85397dbf6feffb5210d47340d
Rene Baine <renebaine13@gmail.com>
2025-12-28 11:58:14 -0500

    function defined in send_mail.py so that the  scheduler.py import works

--- send_mail.py
+++ send_mail.py
@@ -1,18 +1,20 @@
 # Looking to send emails in production? Check out our Email API/SMTP product!
 import smtplib
 
-sender = "Private Person <from@example.com>"
-receiver = "A Test User <to@example.com>"
+def send_mail(to_email, subject, message_body):
+    sender = "private Person <from@example.com>"
+    receiver = to_email
 
-message = f"""\
+    message = f"""\
 Subject: Hello Rene
 To: {receiver}
 From: {sender}
 
-This is a test e-mail message."""
+{message_body}
+"""
 
-with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
-    server.starttls()
-    server.login("8c3356f7e297d5", "0189ca4e4b3107")
-    server.sendmail(sender, receiver, message)
+    with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
+        server.starttls()
+        server.login("8c3356f7e297d5", "0189ca4e4b3107")
+        server.sendmail(sender, receiver, message)
     
